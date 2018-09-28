dd5cca84296d55b71b2faf661838f47ddc8ac810
sushrithas <sushritha.s@cittumkur.org>
2018-09-28 15:09:06 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,6 @@
+//Write a C Program for the following 
+
+1. Read any Two Numbers 
+2. Compute the sum and store it in output variable
+3. Print the result
+
---
+++ simpleintereest.c
@@ -0,0 +1,14 @@
+// C Program to find the simple interest
+// Fill the blank
+
+#include<stdio.h>
+void main()
+{
+float p,t,r,si;
+printf("Enter the value for p,t and r\n");
+________________________________ //Write function to read p,t and r
+________________________________ //Write expression to find simple interest
+printf("The value of simple interest is %f\n",si);
+}
+
+
